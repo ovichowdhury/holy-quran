2df7e32e59e4a529d1b75782e74103182689b225
monirshimul <monirshimul@mongod1.echain.com>
2020-01-02 09:02:27 +0600

    Nav Change

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -44,12 +44,12 @@ class Navbar extends Component {
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse d-flex justify-content-around " id="navbarTogglerDemo01">
-                    <div>
-                    <h3 style={{color: 'white'}}> <img src={rightDivImage} alt="left image" style={{ width: "50px", height: "50px" }}></img> Holy Quran</h3>
-                    </div>
-                    <div>
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0 " >
+                    <div className="collapse navbar-collapse " id="navbarTogglerDemo01">
+                    
+                    <h3 style={{color: 'white', marginRight:"5rem", fontFamily:"'Julius Sans One', sans-serif"}}> <img src={rightDivImage} alt="left image" style={{ width: "45px", height: "45px" }}></img> &nbsp;Holy<span className="display-4" style={{color:"#ff7f00"}}>Q</span>uran</h3>
+                    
+                    
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0 " style={{fontFamily:"'Julius Sans One', sans-serif"}} >
                             <li className="nav-item active">
                                 <Link to='/' style={this.linkStyle}>Home</Link>
                             </li>
@@ -57,14 +57,16 @@ class Navbar extends Component {
                                 <Link to='/about' style={this.linkStyle}>About</Link>
                             </li>
                         </ul>
-                    </div>
+                    
                         {/* <a className="navbar-brand" href="#">Holy Quran</a> */}
                         
                        
-                        <form className="form-inline my-2 my-lg-0">
+                       <form className="form-inline my-2 my-lg-0">
                             <input className="form-control mr-sm-2" type="search" placeholder="Verse index" aria-label="Search" onChange={this.onChange} name="searchText"/>
                             <button className="btn btn-outline-light my-2 my-sm-0" onClick={this.onSubmit}>Search</button>
                         </form>
+                       
+                        
                     </div>
                 </nav>
             </div>
