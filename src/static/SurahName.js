3d1a1418f0b0d6a904a5c31634a520a882b76241
MehediHasan25 <ishanmehadi@gmail.com>
2020-01-02 14:42:06 +0600

    Surah array of objects 1 to 40 added

---
+++ src/static/SurahName.js
@@ -0,0 +1,504 @@
+export const Surah = [
+  {
+    "id": 1,
+    "fIndex": "1",
+    "lIndex": "7",
+    "totalAyat": "7",
+    "surahNameEng": "Al-Faatiha",
+    "surahNameArb": "الفاتحة",
+    "englishTitle": "The Opener",
+    "details": "Makkan",
+    "description": ""
+  },
+  {
+    "id": 2,
+    "fIndex": "8",
+    "lIndex": "293",
+    "totalAyat": "286",
+    "surahNameEng": "Al-Baqarah",
+    "surahNameArb": "البقرة",
+    "englishTitle": "The Cow",
+    "details": "Medinan",
+    "description": ""
+  },
+  {
+    "id": 3,
+    "fIndex": "294",
+    "lIndex": "493",
+    "totalAyat": "200",
+    "surahNameEng": "Al-Imran",
+    "surahNameArb": "آلِ عِمْرَان",
+    "englishTitle": "The Family of Imran",
+    "details": "Madinah",
+    "description": ""
+  },
+
+  {
+    "id": 4,
+    "fIndex": "494",
+    "lIndex": "669",
+    "totalAyat": "176",
+    "surahNameEng": "An-Nisa",
+    "surahNameArb": "ٱلنِّسَاء",
+    "englishTitle": "The Women",
+    "details": "Madinah",
+    "description": ""
+  },
+
+  {
+    "id": 5,
+    "fIndex": "670",
+    "lIndex": "789",
+    "totalAyat": "120",
+    "surahNameEng": "Al-Ma'idah",
+    "surahNameArb": "ٱلْمَائِدَة",
+    "englishTitle": "The Food, The Repast, The Table",
+    "details": "Madinah",
+    "description": ""
+  },
+
+  {
+    "id": 6,
+    "fIndex": "790",
+    "lIndex": "954",
+    "totalAyat": "165",
+    "surahNameEng": "Al-An'am",
+    "surahNameArb": "ٱلْأَنْعَام",
+    "englishTitle": "The Cattle",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 7,
+    "fIndex": "955",
+    "lIndex": "1160",
+    "totalAyat": "206",
+    "surahNameEng": "Al-A'raf",
+    "surahNameArb": "ٱلْأَعْرَاف",
+    "englishTitle": "The Heights",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 8,
+    "fIndex": "1161",
+    "lIndex": "1235",
+    "totalAyat": "75",
+    "surahNameEng": "Al-Anfal",
+    "surahNameArb": "ٱلْأَنْفَال",
+    "englishTitle": "The Spoils of War",
+    "details": "Madinah",
+    "description": ""
+  },
+
+  {
+    "id": 9,
+    "fIndex": "1236",
+    "lIndex": "1364",
+    "totalAyat": "129",
+    "surahNameEng": "At-Tawbah",
+    "surahNameArb": "ٱلتَّوْبَة",
+    "englishTitle": "The Repentance",
+    "details": "Madinah",
+    "description": ""
+  },
+
+  {
+    "id": 10,
+    "fIndex": "1365",
+    "lIndex": "1473",
+    "totalAyat": "109",
+    "surahNameEng": "Yunus",
+    "surahNameArb": "يُونُس",
+    "englishTitle": "Jonah",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 11,
+    "fIndex": "1474",
+    "lIndex": "1596",
+    "totalAyat": "123",
+    "surahNameEng": "Hud",
+    "surahNameArb": "هُود",
+    "englishTitle": "Hud",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 12,
+    "fIndex": "1597",
+    "lIndex": "1707",
+    "totalAyat": "111",
+    "surahNameEng": "Yusuf",
+    "surahNameArb": "يُوسُف",
+    "englishTitle": "Joseph",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 13,
+    "fIndex": "1708",
+    "lIndex": "1750",
+    "totalAyat": "43",
+    "surahNameEng": "Ar-Ra'd",
+    "surahNameArb": "ٱلرَّعْد",
+    "englishTitle": "The Thunder",
+    "details": "Madinah",
+    "description": ""
+  },
+
+  {
+    "id": 14,
+    "fIndex": "1751",
+    "lIndex": "1802",
+    "totalAyat": "52",
+    "surahNameEng": "Ibrahim",
+    "surahNameArb": "إِبْرَاهِيم",
+    "englishTitle": "Abraham",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 15,
+    "fIndex": "1802",
+    "lIndex": "1901",
+    "totalAyat": "99",
+    "surahNameEng": "Al-Hijr",
+    "surahNameArb": "ٱلْحِجْر",
+    "englishTitle": "The Rocky Tract",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 16,
+    "fIndex": "1902",
+    "lIndex": "2029",
+    "totalAyat": "128",
+    "surahNameEng": "An-Nahl",
+    "surahNameArb": "ٱلنَّحْل",
+    "englishTitle": "The Honey Bees, The Bee",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 17,
+    "fIndex": "2030",
+    "lIndex": "2140",
+    "totalAyat": "111",
+    "surahNameEng": "Al-Isra'",
+    "surahNameArb": "ٱلْإِسْرَاء",
+    "englishTitle": "The Night Journey",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 18,
+    "fIndex": "2141",
+    "lIndex": "2250",
+    "totalAyat": "110",
+    "surahNameEng": "Al-Kahf",
+    "surahNameArb": "ٱلْكَهْف",
+    "englishTitle": "The Cave",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 19,
+    "fIndex": "2251",
+    "lIndex": "2348",
+    "totalAyat": "98",
+    "surahNameEng": "Maryam",
+    "surahNameArb": "مَرْيَم",
+    "englishTitle": "Mary",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 20,
+    "fIndex": "2349",
+    "lIndex": "2483",
+    "totalAyat": "135",
+    "surahNameEng": "Ta-Ha",
+    "surahNameArb": "طه",
+    "englishTitle": "Ṭāʾ Hāʾ (a name of Muhammad)",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 21,
+    "fIndex": "2484",
+    "lIndex": "2595",
+    "totalAyat": "112",
+    "surahNameEng": "Al-Anbiya'",
+    "surahNameArb": "ٱلْأَنْبِيَاء",
+    "englishTitle": "The Prophets",
+    "details": "Makkah",
+    "description": ""
+  },
+  {
+    "id": 22,
+    "fIndex": "2596",
+    "lIndex": "2673",
+    "totalAyat": "78",
+    "surahNameEng": "Al-Hajj",
+    "surahNameArb": "ٱلْحَجّ",
+    "englishTitle": "The Pilgrimage, The Hajj",
+    "details": "Madinah",
+    "description": ""
+  },
+
+  {
+    "id": 23,
+    "fIndex": "2674",
+    "lIndex": "2791",
+    "totalAyat": "118",
+    "surahNameEng": "Al-Mu'minun",
+    "surahNameArb": "ٱلْمُؤْمِنُون",
+    "englishTitle": "The Believers",
+    "details": "Makkah",
+    "description": ""
+  },
+  {
+    "id": 24,
+    "fIndex": "2792",
+    "lIndex": "2855",
+    "totalAyat": "64",
+    "surahNameEng": "An-Nur",
+    "surahNameArb": "ٱلنُّور",
+    "englishTitle": "The Light",
+    "details": "Madinah",
+    "description": ""
+  },
+  {
+    "id": 25,
+    "fIndex": "2856",
+    "lIndex": "2932",
+    "totalAyat": "77",
+    "surahNameEng": "Al-Furqan",
+    "surahNameArb": "ٱلْفُرْقَان",
+    "englishTitle": "The Criterion",
+    "details": "Makkah",
+    "description": ""
+  },
+  {
+    "id": 26,
+    "fIndex": "2933",
+    "lIndex": "3159",
+    "totalAyat": "227",
+    "surahNameEng": "Ash-Shu'ara'",
+    "surahNameArb": "ٱلشُّعَرَاء",
+    "englishTitle": "The Poets",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 27,
+    "fIndex": "3160",
+    "lIndex": "3252",
+    "totalAyat": "93",
+    "surahNameEng": "An-Naml",
+    "surahNameArb": "ٱلنَّمْل",
+    "englishTitle": "The Ant, The Ants",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 28,
+    "fIndex": "3253",
+    "lIndex": "3340",
+    "totalAyat": "88",
+    "surahNameEng": "Al-Qasas",
+    "surahNameArb": "ٱلْقَصَص",
+    "englishTitle": "The Narrations, The Stories, The Story",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 29,
+    "fIndex": "3341",
+    "lIndex": "3409",
+    "totalAyat": "69",
+    "surahNameEng": "Al-'Ankabut",
+    "surahNameArb": "ٱلْعَنْكَبُوت",
+    "englishTitle": "The Spider",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 30,
+    "fIndex": "3410",
+    "lIndex": "3469",
+    "totalAyat": "60",
+    "surahNameEng": "Ar-Rum",
+    "surahNameArb": "ٱلرُّوم",
+    "englishTitle": "Rome, Byzantium",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 31,
+    "fIndex": "3470",
+    "lIndex": "3503",
+    "totalAyat": "34",
+    "surahNameEng": "Luqman",
+    "surahNameArb": "لُقْمَان",
+    "englishTitle": "Luqman",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 32,
+    "fIndex": "3504",
+    "lIndex": "3533",
+    "totalAyat": "30",
+    "surahNameEng": "As-Sajdah",
+    "surahNameArb": "ٱلسَّجْدَة",
+    "englishTitle": "The Prostration, Worship, Adoration",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 33,
+    "fIndex": "3534",
+    "lIndex": "3606",
+    "totalAyat": "73",
+    "surahNameEng": "Al-Ahzab",
+    "surahNameArb": "ٱلْأَحْزَاب",
+    "englishTitle": "The Clans, The Confederates, The Combined Forces",
+    "details": "Madinah",
+    "description": ""
+  },
+  {
+    "id": 34,
+    "fIndex": "3607",
+    "lIndex": "3660",
+    "totalAyat": "54",
+    "surahNameEng": "Saba'",
+    "surahNameArb": "سَبَأ",
+    "englishTitle": "Sheba",
+    "details": "Makkah",
+    "description": ""
+  },
+  {
+    "id": 35,
+    "fIndex": "3661",
+    "lIndex": "3705",
+    "totalAyat": "45",
+    "surahNameEng": "Fatir",
+    "surahNameArb": "فَاطِر",
+    "englishTitle": "The Originator",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 36,
+    "fIndex": "3706",
+    "lIndex": "3788",
+    "totalAyat": "83",
+    "surahNameEng": "Ya-Sin",
+    "surahNameArb": "يس",
+    "englishTitle": "Yāʾ Sīn (a name of Muhammad)",
+    "details": "Makkah",
+    "description": ""
+  },
+  {
+    "id": 37,
+    "fIndex": "3789",
+    "lIndex": "3970",
+    "totalAyat": "182",
+    "surahNameEng": "As-Saffat",
+    "surahNameArb": "ٱلصَّافَّات",
+    "englishTitle": "Those Who Set The Ranks, Drawn Up In Ranks, Those Ranged in Ranks",
+    "details": "Makkah",
+    "description": ""
+  },
+  {
+    "id": 38,
+    "fIndex": "3971",
+    "lIndex": "4058",
+    "totalAyat": "88",
+    "surahNameEng": "Saad",
+    "surahNameArb": "ص",
+    "englishTitle": "	Ṣād",
+    "details": "Makkah",
+    "description": ""
+  },
+  {
+    "id": 39,
+    "fIndex": "4059",
+    "lIndex": "4133",
+    "totalAyat": "75",
+    "surahNameEng": "Az-Zumar",
+    "surahNameArb": "ٱلزُّمَر",
+    "englishTitle": "The Crowds, The Troops, Throngs",
+    "details": "Makkah",
+    "description": ""
+  },
+
+  {
+    "id": 40,
+    "fIndex": "4134",
+    "lIndex": "4218",
+    "totalAyat": "85",
+    "surahNameEng": "Ghafir",
+    "surahNameArb": "غَافِر",
+    "englishTitle": "The Forgiver (God), Forgiving",
+    "details": "Makkah",
+    "description": ""
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
