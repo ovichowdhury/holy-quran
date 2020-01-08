bb40c5721c55e9b1167d3c8a004bcb6b359093be
monirshimul <monirshimul@mongod1.echain.com>
2020-01-08 11:29:54 +0600

    Autocomplete Added Two

--- src/components/SearchArea.js
+++ src/components/SearchArea.js
@@ -1,21 +1,11 @@
 import React, { Component } from "react";
 import rightDivImage from "../static/logo1.png";
 // import leftDivImage from '../static/rsz_muhammad.png';
-<<<<<<< Updated upstream
-import { QuranStorageService } from '../services/quranStorageEthService';
-<<<<<<< Updated upstream
-import {withRouter } from 'react-router-dom';
-import {NotificationManager} from 'react-notifications';
-=======
-import { withRouter } from 'react-router-dom';
->>>>>>> Stashed changes
-=======
 import { QuranStorageService } from "../services/quranStorageEthService";
 import { withRouter } from "react-router-dom";
 import { NotificationManager } from "react-notifications";
 
 import { Surah } from "../static/SurahName";
->>>>>>> Stashed changes
 
 class SearchArea extends Component {
   state = {
@@ -62,29 +52,6 @@ class SearchArea extends Component {
     }
   }
 
-<<<<<<< Updated upstream
-    async request(type) {
-        let contractDetails = sessionStorage.getItem('contractDetails');
-        let ayatData;
-        if (contractDetails) {
-            let cd = JSON.parse(contractDetails);
-            let qss = new QuranStorageService(cd);
-            switch (type) {
-                case 'surah':
-                    ayatData = await qss.getBySurah(this.state.surahName, this.state.ayatNumber);
-                    break;
-                case 'para':
-                    ayatData = await qss.getByPara(this.state.para, this.state.ayatNumber);
-                    break;
-            }
-
-            return ayatData
-        }
-        else {
-            alert("Contract Details not Found. Please Reload.");
-            return null;
-        }
-=======
   onChange(e) {
     if (e.target.name === "surahOrPara") {
       let v = parseInt(e.target.value);
@@ -96,7 +63,6 @@ class SearchArea extends Component {
       else this.setState({ para: e.target.value });
     } else {
       this.setState({ [e.target.name]: e.target.value });
->>>>>>> Stashed changes
     }
   }
 
@@ -116,34 +82,8 @@ class SearchArea extends Component {
       match.innerHTML = "";
     }
 
-<<<<<<< Updated upstream
-    async onSearch(e) {
-        try {
-            if (this.state.surahName !== "") {
-                //request for getBySurah
-                let ayatData = await this.request('surah');
-                console.log(ayatData);
-                this.props.history.push('/search', { ayat: ayatData });
-
-            }
-            else if (this.state.para !== "") {
-                // request for getByPara
-                let ayatData = await this.request('para');
-                console.log(ayatData);
-                this.props.history.push('/search', { ayat: ayatData });
-            }
-        }
-        catch (ex) {
-            console.error(ex);
-            //alert("Please check your inputs");
-            NotificationManager.error('Please check your inputs', 'Warning', 5000);
-            
-        }
-
-=======
     this.outPut(matching);
   };
->>>>>>> Stashed changes
 
   // Show Result in Html
   outPut = matchingData => {
@@ -188,45 +128,6 @@ class SearchArea extends Component {
     }
   }
 
-<<<<<<< Updated upstream
-    render() {
-        return (
-            <div className="container">
-                <div className="row d-flex justify-content-center">
-                    <div className="col-sm-8 mt-4" style={{ display: "flex", justifyContent: "center" }}>
-                        <img src={rightDivImage} alt="left image" style={{ width: "150px", height: "150px" }}></img>
-                    </div>
-                    <div className=" col-sm-8 mt-4">
-                        <div className="card shadow" style={{ border: "none" }}>
-                            <div className="card-hedaer">
-
-                            </div>
-                            <div className="card-body">
-                                <form>
-                                    <p className=""> <i className="fas fa-search"></i> &nbsp;Search any ayat here...</p>
-                                    <div className="form-row" >
-                                        <div className="col-7">
-                                            <input type="text" className="form-control" placeholder="Surah Name / Para" name="surahOrPara" onChange={this.onChange} />
-                                        </div>
-                                        <div className="col">
-                                            <input type="text" className="form-control" placeholder="Verse Number" name="ayatNumber" onChange={this.onChange} />
-                                        </div>
-                                        <div className="col">
-                                            <input type="button" className="btn btn-outline-success" value="Search" onClick={this.onSearch} />
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
-            </div>
-
-        )
-    }
-=======
   render() {
     return (
       <div className="container" id="searchBack">
@@ -298,7 +199,6 @@ class SearchArea extends Component {
       </div>
     );
   }
->>>>>>> Stashed changes
 }
 
 export default withRouter(SearchArea);
